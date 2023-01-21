29f379f067c3fb3ae718b4249023bcc903d622c3
veenanc01 <veena.chandrappa@accenture.com>
2023-01-21 23:06:04 +0530

    updated files

---
+++ src/test/java/com/example/codefest/codefestpart1/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codefest.codefestpart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
